8088afc44ea02d2128453c1d7dd784c878cdc230
Lorenzo Milesi <lorenzo.milesi94@gmail.com>
2021-09-17 19:06:28 +0200

    :tada: init boilerplate

---
+++ frontend/src/Theme/themes/default_dark/Images.ts
@@ -0,0 +1,12 @@
+/**
+ * Images should be stored in the `App/Images` directory and referenced using variables defined here.
+ */
+
+export default function () {
+  return {
+    logo: {
+      uri:
+        'https://thecodingmachine.github.io/react-native-hackatomici2021/img/TOM-small.png',
+    },
+  }
+}
