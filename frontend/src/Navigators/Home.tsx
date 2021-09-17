b2f6a4c13ae2b9aa3f12d5e4d1ee15e1a95eaee2
Giorgio Bertolotti <giorgiobertol@gmail.com>
2021-09-17 23:39:51 +0200

    :heavy_plus_sign: Add @types/react-native-vector-icons and start using vector icons in tab navigation

--- frontend/src/Navigators/Home.tsx
+++ frontend/src/Navigators/Home.tsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { IndexExampleContainer, IndexMapContainer } from '@/Containers'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { useTheme } from '@/Theme'
 
 const Tab = createBottomTabNavigator()
 
 // @refresh reset
 const HomeNavigator = () => {
+  const { Colors } = useTheme()
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          const iconName = (() => {
+            switch (route.name) {
+              case 'Map':
+                return focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline'
+              case 'Trips':
+                return focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline'
+              case 'Community':
+                return focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline'
+              case 'Account':
+                return focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline'
+              default:
+                return ''
+            }
+          })()
+
+          return <Icon name={iconName} size={size} color={color} />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: Colors.primary,
+        inactiveTintColor: Colors.text,
+      }}
+    >
       <Tab.Screen name="Map" component={IndexMapContainer} />
       <Tab.Screen name="Trips" component={IndexExampleContainer} />
       <Tab.Screen name="Community" component={IndexExampleContainer} />
