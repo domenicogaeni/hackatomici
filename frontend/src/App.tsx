b660f349b5f882e78b11b786fbef9be221d6161a
Lorenzo Milesi <lorenzo.milesi94@gmail.com>
2021-09-17 21:18:56 +0200

    :lipstick: added NativeBaseProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import 'react-native-gesture-handler'
+import { ApplicationNavigator } from '@/Navigators'
+import { persistor, store } from '@/Store'
+import { NativeBaseProvider } from 'native-base'
 import React from 'react'
+import 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
-import { store, persistor } from '@/Store'
-import { ApplicationNavigator } from '@/Navigators'
 import './Translations'
 
 const App = () => (
-  <Provider store={store}>
-    {/**
-     * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-     * and saved to redux.
-     * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-     * for example `loading={<SplashScreen />}`.
-     * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-     */}
-    <PersistGate loading={null} persistor={persistor}>
-      <ApplicationNavigator />
-    </PersistGate>
-  </Provider>
+  <NativeBaseProvider>
+    <Provider store={store}>
+      {/**
+       * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
+       * and saved to redux.
+       * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
+       * for example `loading={<SplashScreen />}`.
+       * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
+       */}
+      <PersistGate loading={null} persistor={persistor}>
+        <ApplicationNavigator />
+      </PersistGate>
+    </Provider>
+  </NativeBaseProvider>
 )
 
 export default App
--- frontend/src/Containers/Example/Index.tsx
+++ frontend/src/Containers/Example/Index.tsx
@@ -3,17 +3,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import {
   View,
   ActivityIndicator,
-  Text,
   TextInput,
   TouchableOpacity,
 } from 'react-native'
-import { Brand } from '@/Components'
 import { useTheme } from '@/Theme'
 import FetchOne from '@/Store/User/FetchOne'
 import ChangeTheme from '@/Store/Theme/ChangeTheme'
 import { useTranslation } from 'react-i18next'
 import { UserState } from '@/Store/User'
 import { ThemeState } from '@/Store/Theme'
+import { Box, HStack, VStack, Text, Pressable, Image } from 'native-base'
 
 const IndexExampleContainer = () => {
   const { t } = useTranslation()
@@ -43,18 +42,65 @@ const IndexExampleContainer = () => {
 
   return (
     <View style={[Layout.fill, Layout.colCenter, Gutters.smallHPadding]}>
-      <View style={[[Layout.colCenter, Gutters.smallHPadding]]}>
-        <Brand />
-        {fetchOneUserLoading && <ActivityIndicator />}
+      <View style={[[Gutters.smallHPadding]]}>
+        <Box
+          bg="primary.600"
+          py={4}
+          px={3}
+          rounded="md"
+          alignSelf="center"
+          width={375}
+          maxWidth="100%"
+        >
+          <HStack justifyContent="space-between">
+            <Box justifyContent="space-between">
+              <VStack space={2}>
+                <Text fontSize="xxs" color="white">
+                  20/03/2022 {'->'} 22/03/2022
+                </Text>
+                <Text color="white" fontSize="lg">
+                  Barcellona
+                </Text>
+              </VStack>
+              <Pressable
+                rounded="sm"
+                bg="primary.400"
+                alignSelf="flex-start"
+                py={2}
+                px={3}
+              >
+                <Text
+                  textTransform="uppercase"
+                  fontSize={'sm'}
+                  fontWeight="bold"
+                  color="white"
+                >
+                  Visualizza
+                </Text>
+              </Pressable>
+            </Box>
+            <Image
+              source={{
+                uri:
+                  'https://media.vanityfair.com/photos/5ba12e6d42b9d16f4545aa19/3:2/w_1998,h_1332,c_limit/t-Avatar-The-Last-Airbender-Live-Action.jpg',
+              }}
+              alt="Aang flying and surrounded by clouds"
+              height={100}
+              rounded="full"
+              width={100}
+            />
+          </HStack>
+        </Box>
+        {/* {fetchOneUserLoading && <ActivityIndicator />}
         {fetchOneUserError ? (
           <Text style={Fonts.textRegular}>{fetchOneUserError.message}</Text>
         ) : (
           <Text style={Fonts.textRegular}>
             {t('example.helloUser', { name: user.name })}
           </Text>
-        )}
+        )} */}
       </View>
-      <View
+      {/* <View
         style={[
           Layout.row,
           Layout.rowHCenter,
@@ -75,8 +121,8 @@ const IndexExampleContainer = () => {
           selectTextOnFocus
           style={[Layout.fill, Common.textInput]}
         />
-      </View>
-      <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>DarkMode :</Text>
+      </View> */}
+      {/* <Text style={[Fonts.textRegular, Gutters.smallBMargin]}>DarkMode :</Text>
 
       <TouchableOpacity
         style={[Common.button.rounded, Gutters.regularBMargin]}
@@ -95,7 +141,7 @@ const IndexExampleContainer = () => {
         onPress={() => changeTheme({ darkMode: false })}
       >
         <Text style={Fonts.textRegular}>Light</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   )
 }
