0ba546a11ccfcf797b0a87fc48e0cdeca6530d44
Domenico Gaeni <domenicogaeni@gmail.com>
2021-09-18 13:27:22 +0200

    :bug: Fix bug on authentication.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,7 +12,7 @@ class User extends BaseModel
         'institution_place_id'
     ];
 
-    protected array $fillable = [
+    protected $fillable = [
         'name',
         'surname',
         'email'
