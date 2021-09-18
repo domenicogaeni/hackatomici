4292c071d9c622a2d469a9d9a0a27d984c7210e6
Giorgio Bertolotti <giorgiobertol@gmail.com>
2021-09-18 23:22:40 +0200

    :recycle: Replace spinner with ActivityIndicator

--- frontend/src/Containers/TripDetail/Index.tsx
+++ frontend/src/Containers/TripDetail/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { goBack, navigate } from '@/Navigators/utils'
-import { Box, HStack, Pressable, Spinner, Text, VStack } from 'native-base'
+import { Box, HStack, Pressable, Text, VStack } from 'native-base'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { HeaderBackButton } from '@react-navigation/stack'
@@ -14,6 +14,7 @@ import TripStopPlaceHolder from '../TripStopPlaceHolder'
 import TripStopConnector from '../TripStopConnector'
 import TripCircleIcon from '../TripCircleIcon'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { ActivityIndicator } from 'react-native'
 
 const TripDetail = ({ route }: any) => {
   const { tripId } = route.params || {}
@@ -111,7 +112,7 @@ const TripDetail = ({ route }: any) => {
         if (getTripResponse.status === 200) {
           setTrip((await getTripResponse.json()).data)
         }
-      } catch (readTripError) { }
+      } catch (readTripError) {}
 
       setIsLoading(false)
     },
@@ -151,59 +152,61 @@ const TripDetail = ({ route }: any) => {
     ? moment(trip.to, 'YYYY-MM-DD').format('DD/MM/YYYY')
     : 'Oggi'
 
-  return isLoading || !trip ? (
-    <Spinner accessibilityLabel="Carico l'itinerario" />
-  ) : (
+  return (
     <BottomSheetModalProvider>
       <KeyboardAwareScrollView style={{ backgroundColor: 'white' }}>
         <HeaderBackButton onPress={goBack} label="Indietro" />
-        <Box height="100%" width="100%" bg="white" marginBottom={12}>
-          <Box paddingX={8} paddingTop={8}>
-            <HStack
-              justifyContent="space-between"
-              alignItems="center"
-              marginBottom={1}
-            >
-              <Text fontSize="3xl" fontWeight={600}>
-                {trip.name}
-              </Text>
-              <Pressable onPress={addTripStop}>
-                <Icon name="add-circle" size={32} color="#14b8a6" />
-              </Pressable>
-            </HStack>
-            {trip.description && (
-              <Text marginBottom={8} color="gray.500">
-                {trip.description}
-              </Text>
-            )}
-          </Box>
-          <Box paddingX={4}>
-            <HStack alignItems="center">
-              <TripCircleIcon name="play" />
-              <VStack flex={1}>
-                <Text fontSize="xs" color="gray.400">
-                  {formattedFromDate}
+        {isLoading || !trip ? (
+          <ActivityIndicator />
+        ) : (
+          <Box height="100%" width="100%" bg="white" marginBottom={8}>
+            <Box paddingX={8}>
+              <HStack
+                justifyContent="space-between"
+                alignItems="center"
+                marginBottom={1}
+              >
+                <Text fontSize="3xl" fontWeight={600}>
+                  {trip.name}
                 </Text>
-                <Text>Inizio itinerario</Text>
-              </VStack>
-            </HStack>
-            {trip.stops.length > 0 ? (
-              map(trip.stops, (stop, index) => renderStop(stop, index))
-            ) : (
-              <TripStopPlaceHolder onAddStop={addTripStop} />
-            )}
-            <TripStopConnector />
-            <HStack alignItems="center">
-              <TripCircleIcon name="stop" />
-              <VStack flex={1}>
-                <Text fontSize="xs" color="gray.400">
-                  {formattedToDate}
+                <Pressable onPress={addTripStop}>
+                  <Icon name="add-circle" size={32} color="#14b8a6" />
+                </Pressable>
+              </HStack>
+              {trip.description && (
+                <Text marginBottom={8} color="gray.500">
+                  {trip.description}
                 </Text>
-                <Text>Fine itinerario</Text>
-              </VStack>
-            </HStack>
+              )}
+            </Box>
+            <Box paddingX={4}>
+              <HStack alignItems="center">
+                <TripCircleIcon name="play" />
+                <VStack flex={1}>
+                  <Text fontSize="xs" color="gray.400">
+                    {formattedFromDate}
+                  </Text>
+                  <Text>Inizio itinerario</Text>
+                </VStack>
+              </HStack>
+              {trip.stops.length > 0 ? (
+                map(trip.stops, (stop, index) => renderStop(stop, index))
+              ) : (
+                <TripStopPlaceHolder onAddStop={addTripStop} />
+              )}
+              <TripStopConnector />
+              <HStack alignItems="center">
+                <TripCircleIcon name="stop" />
+                <VStack flex={1}>
+                  <Text fontSize="xs" color="gray.400">
+                    {formattedToDate}
+                  </Text>
+                  <Text>Fine itinerario</Text>
+                </VStack>
+              </HStack>
+            </Box>
           </Box>
-        </Box>
+        )}
       </KeyboardAwareScrollView>
     </BottomSheetModalProvider>
   )
