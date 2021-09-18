f2eb33d7906b351bcc6e0e74476b86d2eae3200d
Domenico Gaeni <domenicogaeni@gmail.com>
2021-09-18 16:52:27 +0200

    :sparkles: Add crud trip.

---
+++ backend/app/Http/Controllers/TripController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trip;
+use App\Models\TripPoint;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TripController extends BaseController
+{
+    public static function getValidationRules()
+    {
+        return array_merge(parent::getValidationRules(), [
+            'new' => [
+                'name' => 'required|string',
+                'description' => 'filled|string',
+                'from' => 'required|date',
+                'to' => 'required|date',
+            ],
+            'edit' => [
+                'name' => 'string',
+                'description' => 'string',
+                'from' => 'date',
+                'to' => 'date',
+            ],
+        ]);
+    }
+
+    public function new(Request $request)
+    {
+        $currentUser = Auth::user();
+        $fillable = $request->only(['name', 'description', 'from', 'to']);
+
+        $trip = new Trip();
+        $trip->user_id = $currentUser->id;
+        $trip->fill($fillable);
+        
+        $trip->save();
+    }
+
+    public function edit(Request $request, $tripId)
+    {
+        $currentUser = Auth::user();
+        $fillable = $request->only(['name', 'description', 'from', 'to']);
+
+        $trip = Trip::where('user_id', $currentUser->id)->findOrFail($tripId);
+        $trip->fill($fillable);
+        
+        $trip->save();
+    }
+
+    public function delete($tripId)
+    {
+        $currentUser = Auth::user();        
+
+        $trip = Trip::where('user_id', $currentUser->id)->findOrFail($tripId);
+        
+        TripPoint::where('trip_id', $trip->id)->delete();
+        $trip->delete();
+    }
+}
