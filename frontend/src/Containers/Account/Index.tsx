9ac73ebe23534d830cbcc9e6bc988ebacf29ec61
Giorgio Bertolotti <giorgiobertol@gmail.com>
2021-09-19 14:51:12 +0200

    :sparkles: Generate and send the correct FCM token

--- frontend/src/Containers/Account/Index.tsx
+++ frontend/src/Containers/Account/Index.tsx
@@ -5,7 +5,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import Logout from '@/Store/User/Logout'
 import { UserState } from '@/Store/User'
 import { navigate } from '@/Navigators/utils'
-import ClearDeviceId from '@/Store/DeviceId/ClearDeviceId'
 
 const Account = () => {
   const dispatch = useDispatch()
@@ -17,7 +16,6 @@ const Account = () => {
   const logout = useCallback(() => {
     auth().signOut()
     dispatch(Logout.action())
-    dispatch(ClearDeviceId.action())
   }, [dispatch])
 
   const displayName = `${user?.name} ${
--- frontend/src/Store/DeviceId/index.ts
+++ frontend/src/Store/DeviceId/index.ts
@@ -1,18 +1,11 @@
 import { buildSlice } from '@thecodingmachine/redux-toolkit-wrapper'
-import ClearDeviceId from './ClearDeviceId'
 import SendDeviceId from './SendDeviceId'
-import SetDeviceId from './SetDeviceId'
 
-export default buildSlice('deviceId', [
-  SendDeviceId,
-  SetDeviceId,
-  ClearDeviceId,
-]).reducer
+export default buildSlice('deviceId', [SendDeviceId]).reducer
 
 export interface DeviceIdState {
   send: {
     loading: boolean
     error: any
   }
-  deviceId?: string
 }
